7b3f45102393fcb02ce18c8bd24b46de2fec7676
Naveed Aheer <naveedaheer@gmail.com>
2020-03-10 12:25:03 +0500

    Store initial beverages list on first load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SaloonService } from './services/saloon.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-saloon';
   dataRemoved = false;
+  initialBeveragesList = ["Coffee", "Milk", "Water", "Tea"];
+  constructor(private service: SaloonService) {
+
+  }
+
+  /**
+ * store default beverage list
+ */
+  ngOnInit(): void {
+    if (!(this.service.getBeverages().length)) {
+      this.service.saveBeverages(this.initialBeveragesList);
+    }
+  }
 
   /**
    * @param event true if all the data has been removed
